8da0f646a5c8bce5aeac70728c233d661b11976e
Florian Weber <f.weber@digi-info.de>
2013-03-20 21:36:43 +0100

    Init imageoptim module.

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# Public: Install ImageOptim.app into /Applications.
+#
+# Examples
+#
+#   include imageoptim
+#
+class imageoptim {
+  package { 'ImageOptim':
+    provider => 'compressed_app',
+    source   => 'http://imageoptim.com/ImageOptim.tbz2'
+  }
+}
---
+++ spec/classes/imageoptim_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'imageoptim' do
+  it do
+    should contain_package('ImageOptim').with({
+      :provider => 'compressed_app',
+      :source   => 'http://imageoptim.com/ImageOptim.tbz2',
+    })
+  end
+end
